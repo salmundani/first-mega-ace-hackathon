6a3bbad5b7ccd93cadfd416d45ebcb5efe668e62
Daniel Salmun <salmundani@gmail.com>
2023-04-16 14:53:26 -0300

    Tic Tac Toe tests passing

--- TicTacToe/tic_tac_toe.py
+++ TicTacToe/tic_tac_toe.py
@@ -5,14 +5,14 @@ from pyteal import *
 #[1, 5, 9], [3, 5, 7]              # Diagonals
 @Subroutine(TealType.uint64)
 def check_win(player, pos1: ScratchVar, pos2: ScratchVar, pos3: ScratchVar, pos4: ScratchVar, pos5: ScratchVar, pos6: ScratchVar, pos7: ScratchVar, pos8: ScratchVar, pos9: ScratchVar):
-    return If(Or(And(pos1.load() == player, pos2.load() == player, pos3.load() == player),
+    return Or(And(pos1.load() == player, pos2.load() == player, pos3.load() == player),
        And(pos4.load() == player, pos5.load() == player, pos6.load() == player),
        And(pos7.load() == player, pos8.load() == player, pos9.load() == player),
        And(pos1.load() == player, pos4.load() == player, pos7.load() == player),
        And(pos2.load() == player, pos5.load() == player, pos8.load() == player),
        And(pos3.load() == player, pos6.load() == player, pos9.load() == player),
        And(pos1.load() == player, pos5.load() == player, pos9.load() == player),
-       And(pos3.load() == player, pos5.load() == player, pos7.load() == player))).Then(player).Else(Int(0))
+       And(pos3.load() == player, pos5.load() == player, pos7.load() == player))
     
 
 def tic_tac_toe():
@@ -29,9 +29,9 @@ def tic_tac_toe():
     pos8 = ScratchVar(TealType.uint64, 8)
     pos9 = ScratchVar(TealType.uint64, 9)
     i = ScratchVar(TealType.uint64)
-    j = ScratchVar(TealType.uint64)
     moved = ScratchVar(TealType.uint64)
     op_corner = ScratchVar(TealType.uint64)
+    tmp_pos = ScratchVar(TealType.uint64)
     pos = DynamicScratchVar(TealType.uint64)
     return Seq(
         status.store(Bytes("playing")),
@@ -58,56 +58,104 @@ def tic_tac_toe():
             .ElseIf(GetByte(player_moves, i.load()) == Int(8)).Then(pos.set_index(pos8))
             .ElseIf(GetByte(player_moves, i.load()) == Int(9)).Then(pos.set_index(pos9), op_corner.store(Int(1)))
             .Else(status.store(Bytes("bad move")), Break()),
-            If(Not(pos.load())).Then(pos.store(Int(1))).Else(status.store(Bytes("bad move")), Break()),
-            If(check_win(Int(1), pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9), status.store(Bytes("player won")), Break()),
-            If(i.load() == Int(4), status.store(Bytes("draw")), Break()),
+            If(Not(pos.load())).Then(pos.store(Int(1))).Else(
+                status.store(Bytes("bad move")), 
+                Break()
+            ),
+            If(check_win(Int(1), pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9)).Then(
+                status.store(Bytes("player won")),
+                Break()
+            ),
+            If(i.load() == Int(4)).Then(status.store(Bytes("draw")), Break()),
             
             # Contract moves
             # 1: If contract has two in a row -> win
-            For(j.store(Int(1)), j.load() < Int(10), j.store(j.load() + Int(1))).Do(
-                If(j.load() == Int(1)).Then(pos.set_index(pos1))
-                .ElseIf(j.load() == Int(2)).Then(pos.set_index(pos2))
-                .ElseIf(j.load() == Int(3)).Then(pos.set_index(pos3))
-                .ElseIf(j.load() == Int(4)).Then(pos.set_index(pos4))
-                .ElseIf(j.load() == Int(5)).Then(pos.set_index(pos5))
-                .ElseIf(j.load() == Int(6)).Then(pos.set_index(pos6))
-                .ElseIf(j.load() == Int(7)).Then(pos.set_index(pos7))
-                .ElseIf(j.load() == Int(8)).Then(pos.set_index(pos8))
-                .Else(pos.set_index(pos9)),
-                If(Not(pos.load())).Then(
-                    pos.store(Int(2)),
-                    If(check_win(Int(2), pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9)).Then(
-                        moved.store(Int(1)),
-                        Break()
-                    ).Else(
-                        pos.store(Int(0))
-                    )
-                )
+            tmp_pos.store(Int(2)),
+            If(And(Not(pos1.load()),
+                   check_win(Int(2), tmp_pos, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9))).Then(
+                pos1.store(Int(2)),
+                status.store(Bytes("contract wins")),
+                Break()
+            ).ElseIf(And(Not(pos2.load()),
+                   check_win(Int(2), pos1, tmp_pos, pos3, pos4, pos5, pos6, pos7, pos8, pos9))).Then(
+                pos2.store(Int(2)),
+                status.store(Bytes("contract wins")),
+                Break()
+            ).ElseIf(And(Not(pos3.load()),
+                   check_win(Int(2), pos1, pos2, tmp_pos, pos4, pos5, pos6, pos7, pos8, pos9))).Then(
+                pos3.store(Int(2)),
+                status.store(Bytes("contract wins")),
+                Break()
+            ).ElseIf(And(Not(pos4.load()),
+                   check_win(Int(2), pos1, pos2, pos3, tmp_pos, pos5, pos6, pos7, pos8, pos9))).Then(
+                pos4.store(Int(2)),
+                status.store(Bytes("contract wins")),
+                Break()
+            ).ElseIf(And(Not(pos5.load()),
+                   check_win(Int(2), pos1, pos2, pos3, pos4, tmp_pos, pos6, pos7, pos8, pos9))).Then(
+                pos5.store(Int(2)),
+                status.store(Bytes("contract wins")),
+                Break()
+            ).ElseIf(And(Not(pos6.load()),
+                   check_win(Int(2), pos1, pos2, pos3, pos4, pos5, tmp_pos, pos7, pos8, pos9))).Then(
+                pos6.store(Int(2)),
+                status.store(Bytes("contract wins")),
+                Break()
+            ).ElseIf(And(Not(pos7.load()),
+                   check_win(Int(2), pos1, pos2, pos3, pos4, pos5, pos6, tmp_pos, pos8, pos9))).Then(
+                pos7.store(Int(2)),
+                status.store(Bytes("contract wins")),
+                Break()
+            ).ElseIf(And(Not(pos8.load()),
+                   check_win(Int(2), pos1, pos2, pos3, pos4, pos5, pos6, pos7, tmp_pos, pos9))).Then(
+                pos8.store(Int(2)),
+                status.store(Bytes("contract wins")),
+                Break()
+            ).ElseIf(And(Not(pos9.load()),
+                   check_win(Int(2), pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, tmp_pos))).Then(
+                pos9.store(Int(2)),
+                status.store(Bytes("contract wins")),
+                Break()
             ),
-
+            
             # 2: If player has two in a row -> block
-            If(Not(moved.load())).Then(
-                For(j.store(Int(1)), j.load() < Int(10), j.store(j.load() + Int(1))).Do(
-                    If(j.load() == Int(1)).Then(pos.set_index(pos1))
-                    .ElseIf(j.load() == Int(2)).Then(pos.set_index(pos2))
-                    .ElseIf(j.load() == Int(3)).Then(pos.set_index(pos3))
-                    .ElseIf(j.load() == Int(4)).Then(pos.set_index(pos4))
-                    .ElseIf(j.load() == Int(5)).Then(pos.set_index(pos5))
-                    .ElseIf(j.load() == Int(6)).Then(pos.set_index(pos6))
-                    .ElseIf(j.load() == Int(7)).Then(pos.set_index(pos7))
-                    .ElseIf(j.load() == Int(8)).Then(pos.set_index(pos8))
-                    .Else(pos.set_index(pos9)),
-                    If(Not(pos.load())).Then(
-                        pos.store(Int(1)),
-                        If(check_win(Int(1), pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9)).Then(
-                            pos.store(Int(2)),
-                            moved.store(Int(1)),
-                            Break()
-                        ).Else(
-                            pos.store(Int(0))
-                        )
-                    )
-                )
+            tmp_pos.store(Int(1)),
+            If(And(Not(pos1.load()),
+                check_win(Int(1), tmp_pos, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9))).Then(
+                pos1.store(Int(2)),
+                moved.store(Int(1))
+            ).ElseIf(And(Not(pos2.load()),
+                check_win(Int(1), pos1, tmp_pos, pos3, pos4, pos5, pos6, pos7, pos8, pos9))).Then(
+                pos2.store(Int(2)),
+                moved.store(Int(1))
+            ).ElseIf(And(Not(pos3.load()),
+                check_win(Int(1), pos1, pos2, tmp_pos, pos4, pos5, pos6, pos7, pos8, pos9))).Then(
+                pos3.store(Int(2)),
+                moved.store(Int(1))
+            ).ElseIf(And(Not(pos4.load()),
+                check_win(Int(1), pos1, pos2, pos3, tmp_pos, pos5, pos6, pos7, pos8, pos9))).Then(
+                pos4.store(Int(2)),
+                moved.store(Int(1))
+            ).ElseIf(And(Not(pos5.load()),
+                check_win(Int(1), pos1, pos2, pos3, pos4, tmp_pos, pos6, pos7, pos8, pos9))).Then(
+                pos5.store(Int(2)),
+                moved.store(Int(1))
+            ).ElseIf(And(Not(pos6.load()),
+                check_win(Int(1), pos1, pos2, pos3, pos4, pos5, tmp_pos, pos7, pos8, pos9))).Then(
+                pos6.store(Int(2)),
+                moved.store(Int(1))
+            ).ElseIf(And(Not(pos7.load()),
+                check_win(Int(1), pos1, pos2, pos3, pos4, pos5, pos6, tmp_pos, pos8, pos9))).Then(
+                pos7.store(Int(2)),
+                moved.store(Int(1))
+            ).ElseIf(And(Not(pos8.load()),
+                check_win(Int(1), pos1, pos2, pos3, pos4, pos5, pos6, pos7, tmp_pos, pos9))).Then(
+                pos8.store(Int(2)),
+                moved.store(Int(1))
+            ).ElseIf(And(Not(pos9.load()),
+                check_win(Int(1), pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, tmp_pos))).Then(
+                pos9.store(Int(2)),
+                moved.store(Int(1))
             ),
 
             # 3: If the center is open -> fill pos5
@@ -163,15 +211,14 @@ def tic_tac_toe():
                 )
             ),
 
-            Assert(moved.load() == Int(1)),
+            Assert(moved.load()),
 
-            # 7: Check if contract won
-            If(check_win(Int(2), pos1, pos2, pos3, pos4, pos5, pos6, pos7, pos8, pos9), status.store(Bytes("contract won")), Break()),
-            
             # Clean temp vars
             moved.store(Int(0)),
             op_corner.store(Int(0))
-    ), Int(0))
+        ),
+        Int(0)
+    )
 
 if __name__ == "__main__":
     program = tic_tac_toe()
